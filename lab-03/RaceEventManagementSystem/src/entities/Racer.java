b97ab224371f8a56ebeddd0998503497ab3d8d2a
tegu1365 <tedi.d.petkova@gmail.com>
2024-03-14 19:29:51 +0200

    Racer Repository init

--- lab-03/RaceEventManagementSystem/src/entities/Racer.java
+++ lab-03/RaceEventManagementSystem/src/entities/Racer.java
@@ -6,4 +6,35 @@ public class Racer {
     private String lastName;
     private Integer age;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
--- lab-03/RaceEventManagementSystem/src/repositories/RacerRepository.java
+++ lab-03/RaceEventManagementSystem/src/repositories/RacerRepository.java
@@ -1,5 +1,43 @@
 package repositories;
 
-public class RacerRepository {
+import entities.Racer;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class RacerRepository {
+    // structure to store your Racers
+    private static Map<Integer, Racer> racerTable = new HashMap<Integer,Racer>();
+    private static Integer sequence = 0;
+
+    /**
+     * Add racer to your DB
+     * @param racer
+     */
+    public void addRacer(Racer racer) {
+        if(racer.getId()!=null) {
+            throw new IllegalArgumentException("Cannot create Racer with already given ID.");
+        }
+        racer.setId(sequence++);
+        racerTable.put(racer.getId(), racer);
+    }
+
+    /**
+     * Delete racer by id. If there is no element to be deleted then return false;
+     * @param id
+     * @return if there is element to delete -> true, if not -> false
+     */
+    public boolean deleteRacerById(Integer id) {
+        return racerTable.remove(id) == null ? false : true;
+    }
+
+    /**
+     * Get racer by passed id. If there is no element return Optional empty
+     * @param id
+     * @return Optional of Racer
+     */
+    public Optional<Racer> getRacerById(Integer id) {
+        return Optional.of(racerTable.get(id));
+    }
 }
