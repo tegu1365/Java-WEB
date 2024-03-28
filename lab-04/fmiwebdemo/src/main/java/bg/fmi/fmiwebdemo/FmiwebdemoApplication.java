89a7296b25d0e2e64811bc139f0cb2af141d2caa
tegu1365 <tedi.d.petkova@gmail.com>
2024-03-28 18:18:48 +0200

    Push before lab-5

---
+++ lab-04/fmiwebdemo/src/main/java/bg/fmi/fmiwebdemo/FmiwebdemoApplication.java
@@ -0,0 +1,13 @@
+package bg.fmi.fmiwebdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmiwebdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmiwebdemoApplication.class, args);
+	}
+
+}
---
+++ lab-04/fmiwebdemo/src/test/java/bg/fmi/fmiwebdemo/FmiwebdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.fmi.fmiwebdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmiwebdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
